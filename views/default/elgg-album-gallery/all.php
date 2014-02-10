d20a01ac6c9428e847eeedc3ae57508e399f7535
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-02-10 11:51:33 +0100

    Update all.php

--- views/default/elgg-album-gallery/all.php
+++ views/default/elgg-album-gallery/all.php
@@ -1,4 +1,4 @@
 <div class="gallery-all">
-    <h3 class="titolino-all"><a href="<?php echo elgg_get_site_url().'/elgg-album-gallery/'.$vars['guid_user'].'/album/'.$vars['album_guid'];?>"><?php echo $vars['title'];?></a></h3>
+    <h3 class="titolino-all"><a href="<?php echo elgg_get_site_url().'elgg-album-gallery/'.$vars['guid_user'].'/album/'.$vars['album_guid'];?>"><?php echo $vars['title'];?></a></h3>
     <p class="descr-all"><?php echo $vars['descr']; ?></p>
 </div>
